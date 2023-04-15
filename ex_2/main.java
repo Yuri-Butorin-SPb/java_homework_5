2cb29925a926d8f9e3a5114d29e7ec7407f74b65
Yuri <butor.yuri@gmail.com>
2023-04-15 14:48:54 +0300

    Add second exercise

---
+++ ex_2/main.java
@@ -0,0 +1,46 @@
+package ex_2;
+
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args) {
+        String workers = "Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, " +
+                "Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, " +
+                "Марина Светлова, Мария Савина, Мария Рыкова, Марина Лугова, Анна Владимирова, " +
+                "Иван Мечников, Петр Петин, Иван Ежов.";
+        workers.replace(".", "");
+
+        String[] array = workers.split(", ");
+        List<String> names = new ArrayList<>();
+        List<Integer> freq = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            String name = array[i].split(" ")[0];
+            if (names.contains(name)) {
+                int index = names.indexOf(name);
+                freq.set(index, freq.get(index) + 1);
+            } else {
+                names.add(name);
+                freq.add(1);
+            }
+        }
+
+        Map<Integer, List<String>> map = new TreeMap<>();
+
+        for (int i = 0; i < names.size(); i++) {
+            if (map.containsKey(freq.get(i))) {
+                List<String> list = map.get(freq.get(i));
+                list.add(names.get(i));
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(names.get(i));
+                map.put(freq.get(i), list);
+            }
+        }
+
+        System.out.println(map);
+    }
+}
